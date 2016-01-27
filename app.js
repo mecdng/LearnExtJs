33acdfb8b36b2b6622c495297eb429d8b2e6ac33
Yonas Woldemariam <ywoldemariam@zynx.com>
2016-01-26 16:27:20 -0800

    Yonas - basic MVC architcture layout

---
+++ app.js
@@ -0,0 +1,21 @@
+﻿Ext.application({
+    requires: ['Ext.container.Viewport'],
+    name: 'AM',
+
+   controllers: [
+        'Users'
+    ],
+   launch: function () {
+       console.log('applicaiton is launched.');
+        Ext.create('Ext.container.Viewport', {
+            layout: 'fit',
+            items: [
+                {
+                    xtype: 'panel',
+                    title: 'Users',
+                    html: 'List of users will go here'
+                }
+            ]
+        });
+    }
+});
---
+++ app/controller/Users.js
@@ -0,0 +1,15 @@
+﻿Ext.define('AM.controller.Users', {
+    extend: 'Ext.app.Controller',
+    init: function() {
+        console.log('Iniitialized Users! This happens before the Application launch function is called');
+        this.control({
+            'viewport > panel': {
+                render:this.onPanelRendered
+            }
+        });
+    },
+
+    onPanelRendered : function() {
+        console.log('This panel was rendered');
+    }
+})
